ccb3c49ddf59ae32437403cceef8f7f0c94b9055
elprofedotti <elprofedotti@gmail.com>
2023-02-21 22:42:43 -0300

    ejercicio tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numeroIf=-5;
+        if(numeroIf>0){
+            System.out.println("la variable es positiva");
+        }
+        else{
+            if(numeroIf<0){
+                System.out.println("la variable es negativa");
+            }
+            else{
+                System.out.println("la variable es CERO");
+            }
+        }
+        //System.out.println("Hello world!");
     }
 }

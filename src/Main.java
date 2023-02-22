b6d27ebf5025768ea26318285d6f428103a7669d
elprofedotti <elprofedotti@gmail.com>
2023-02-21 23:16:56 -0300

    ejercicio tema 4--> terminado!

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,59 @@
 public class Main {
     public static void main(String[] args) {
         int numeroIf=-5;
+        System.out.println("*******************IF / ELSE***********************************");
+        System.out.println("A la variable se le asigno el valor -->"+numeroIf);
+        System.out.println("");
         if(numeroIf>0){
-            System.out.println("la variable es positiva");
+            System.out.println("la variable numeroIf es positiva");
         }
         else{
             if(numeroIf<0){
-                System.out.println("la variable es negativa");
+                System.out.println("la variable numeroIf es negativa");
             }
             else{
-                System.out.println("la variable es CERO");
+                System.out.println("la variable numeroIf es CERO");
             }
         }
-        //System.out.println("Hello world!");
+
+        System.out.println("***********************WHILE********************************");
+        int numerowhile=0;
+        System.out.println("La variable numeroWhile es: "+numerowhile);
+        while (numerowhile<3){
+            numerowhile++;
+            System.out.println("numeroWhile ahora es: "+numerowhile);
+        }
+
+        System.out.println("************************DOWHILE******************************");
+        numerowhile=0;
+        do{
+            numerowhile++;
+            System.out.println("La variable numeroWhile es: "+numerowhile);
+        }while (numerowhile<1);
+
+        System.out.println("***************************FOR*******************************");
+        for (int numeroFor=0; numeroFor<=3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+        System.out.println("***************************SWITCH*******************************");
+        String estacion="kjhgkihuj";
+        switch (estacion){
+            case "Otoño":
+                System.out.println("Estamos en "+estacion);
+                break;
+            case "Invierno":
+                System.out.println("Estamos en "+estacion);
+                break;
+            case "Primavera":
+                System.out.println("Estamos en "+estacion);
+                break;
+            case "Verano":
+                System.out.println("Estamos en "+estacion);
+                break;
+            default:
+                System.out.println(estacion+" --> No corresponde a una estacion");
+                break;
+        }
+
     }
 }
